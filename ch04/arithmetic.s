f5f5f59039f9e4b44590cb93ef83e96250dc178a
Greg Tate <gregt8@gmail.com>
2024-03-11 15:03:41 -0500

    Chapter 4 work

--- ch04/arithmetic.s
+++ ch04/arithmetic.s
@@ -7,12 +7,12 @@ _start:
     movq $3, %rdi   # %rdi = 3
     movq %rdi, %rax # %rdi = 3, %rax = 3
     addq %rdi, %rax # %rdi = 3, %rax = 6
-    mulq %rdi       # %rdi = 9, %rax = 6
+    mulq %rdi       # %rdi = 3, %rax = 18
     movq $2, %rdi   # %rdi = 2
-    addq %rdi, %rax # %rdi = 2, %rax = 8
-    movq $4, %rdi   # %rdi = 4, %rax = 8
-    mulq %rdi       # %rdi = 16, %rax = 8
-    movq %rax, %rdi # %rdi = 8, %rax = 8
+    addq %rdi, %rax # %rdi = 2, %rax = 20
+    movq $4, %rdi   # %rdi = 4, %rax = 20
+    mulq %rdi       # %rdi = 4, %rax = 80
+    movq %rax, %rdi # %rdi = 80, %rax = 80
 
     # Set the exit system call number
     movq $60, %rax  # %rax = 60
